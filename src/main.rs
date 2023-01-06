ea73f4a6652d997ac109da677af3fcdeb61875cd
Peter Eneroth <peter@anderroth.se>
2023-01-06 11:34:08 +0100

    mutable reference

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,30 @@ fn main() {
     println!("guess_f är: {guess_f}");
     println!("x1 är: {x1}");
     println!("x2 är: {x2}");
-    
-    let s = String::from("Hello");
-    let s2 = append_string(s);
-    println!("s2 = {}",s2);
 
     // Evaluate ownership
-    let s1 = String::from("hello"); 
-    let s2 = s1;
 
-    println!("String = {}",&s2);
+    // Default function call
+    {
+        let s1 = String::from("Hello");
+        let s2 = append_string(s1);  
+        // println!("s1 = {}",s1); // s have ceased to exist
+        println!("s2 = {}",s2);
+    }
+    // Try to reasign
+    {
+        let mut s1 = String::from("Hello");
+        s1 = append_string(s1);  
+        println!("s1 = {}",s1);
+    }
+
+    // Try to mutable references
+    {
+        let mut s1 = String::from("Hello");
+        append_string_ref(&mut s1);  
+        println!("s1 = {}",s1);
+    }
+
 
 }
 
@@ -27,4 +41,8 @@ fn plus_one(x: i32) -> i32 {
 fn append_string(mut s:String) -> String {
     s.push_str(", world!");
     s
+}
+
+fn append_string_ref(s: &mut String) {
+    s.push_str(", world!");
 }
